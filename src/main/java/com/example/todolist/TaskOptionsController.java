a85e047868aa3636f03df6aadfd0c745f9bff7dd
remgagagali727 <remgagagali@outlook.com>
2025-01-07 21:05:49 -0600

    Using an Exception

--- src/main/java/com/example/todolist/TaskOptionsController.java
+++ src/main/java/com/example/todolist/TaskOptionsController.java
@@ -2,12 +2,15 @@ package com.example.todolist;
 
 import com.example.customItems.DoingButton;
 import com.example.customItems.ToDoButton;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class TaskOptionsController {
@@ -24,10 +27,14 @@ public class TaskOptionsController {
         ((Stage) confirmar.getScene().getWindow()).close();
         VBox container = ((VBox) confirmar.getParent().getParent());
         Slider slider = ((Slider) container.getChildren().get(2));
-        TextField textField = ((TextField) container.getChildren().get(4));
-        String newName = textField.getText();
+        String newName;
         //Exception
-        if(newName.length() >= 30) throw new RuntimeException("La longitud del nombre de la tarea es muy larga");
+        try{
+            newName = getTextFieldText(container.getChildren(), 4);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+            newName = "Tarea Default";
+        }
         ToDoButton currButton = HelloController.toDoList.get(HelloController.idToDo);
         currButton.setPriority(slider.getValue());
         currButton.adjustColor();
@@ -39,6 +46,16 @@ public class TaskOptionsController {
         HelloController.idToDo = HelloController.toDoList.indexOf(currButton);
         HelloController.visualizationValue = HelloController.idToDo / 3;
         helloController.resetAllVBox();
+    }
+
+    private String getTextFieldText(ObservableList<Node> children, int i) throws IllegalArgumentException{
+        if(!(children.get(i) instanceof TextField)) throw new IllegalArgumentException("No es un textField");
+        TextField textField = ((TextField) children.get(i));
+        String name = textField.getText();
+        if(name.length() > 30) {
+            throw new IllegalArgumentException("La longitud del nombre excede la permitida");
+        }
+        return name;
     }
 
     @FXML
